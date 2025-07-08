c7faa1c7f6f2ba535a7e9f5b3eda64218cbbd7e3
siddhartha-1028 <siddharthapasumarthy07@gmail.com>
2025-07-08 14:44:35 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative_AI_Project',
+    version='0.0.0',
+    author='siddhartha pasumarthy',
+    author_email='siddharthapasumarthy07@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
